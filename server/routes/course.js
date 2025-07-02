8845ba2266ed3f9c788b42a06f4fba7711332076
Muskan Goyal <goyalmuskan210@gmail.com>
2025-07-02 18:24:13 +0530

    started doing frontend part

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses } from "../controllers/course.js";
+import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses, checkout, paymentVerification } from "../controllers/course.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
@@ -10,6 +10,8 @@ router.get("/lectures/:id", isAuth, fetchLectures);
 router.get("/lecture/:id", isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
 router.get("/mycourse", isAuth, getMyCourses);
+router.post("/course/checkout/:id", isAuth, checkout);
+router.post("/verification/:id", isAuth, paymentVerification);
 
 
 export default router;
