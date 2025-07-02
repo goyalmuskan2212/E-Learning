cb5f312e62d494fb9facd2f5ac01f8224ec5397d
Muskan Goyal <goyalmuskan210@gmail.com>
2025-07-02 20:17:14 +0530

    testimonials added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
-    <div>Home</div>
+    <div>
+      <div className="home">
+        <div className="home-content">
+          <h1>Welcome to our E-learning Platform</h1>
+          <p>Learn, Grow, Excel</p>
+          <button onClick={()=>navigate("/courses")} 
+          className="common-btn">Get Started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
