3ba50d13f0c6141d6b94058615b5497ea723643f
Muskan Goyal <goyalmuskan210@gmail.com>
2026-01-14 21:54:30 +0530

    change in file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
 //import Razorpay from "razorpay";
@@ -43,3 +43,40 @@ connectDb();
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+*/
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDb } from "./database/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// root route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+// static files
+app.use("/uploads", express.static("uploads"));
+
+// routes
+import userRoutes from "./routes/user.js";
+import courseRoutes from "./routes/course.js";
+import adminRoutes from "./routes/admin.js";
+
+app.use("/api", userRoutes);
+app.use("/api", courseRoutes);
+app.use("/api", adminRoutes);
+
+// connect DB
+connectDb();
+
+// 🚨 THIS IS REQUIRED FOR VERCEL
+export default app;
+
